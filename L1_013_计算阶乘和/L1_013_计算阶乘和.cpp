0e7ffc8c25ac971ffab888bf2ba699c66b0d0bcb
ElonKou <elonkou@ktime.cc>
2018-01-19 22:58:12 +0800

    Add L1_013 计算阶乘之和

---
+++ L1_013_计算阶乘和/L1_013_计算阶乘和.cpp
@@ -0,0 +1,32 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_013_计算阶乘和.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Fri 19 Jan 2018 10:42:22 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+	// 这题太简单
+	int sum = 0;
+	int count = 0;
+	cin >> count;
+	int temp = count;
+	int subTemp = 0;
+
+	// 两个循环分别计算求和和求阶乘
+	while(temp>0){
+		int subTemp = temp--;
+		int sumTemp = 1;
+		while(subTemp>0){
+			sumTemp *= subTemp--;
+		}
+		sum +=sumTemp;
+	}
+	cout << sum;
+    return 0;
+}
+
