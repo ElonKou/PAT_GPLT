072280ccbf0114f14ce8147c4d08d36a643feabc
ElonKou <elonkou@ktime.cc>
2018-01-20 01:45:25 +0800

    Add the La_014 简单题

---
+++ L1_014_简单题/L1_014_简单题.cpp
@@ -0,0 +1,15 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_014_简单题.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Fri 19 Jan 2018 10:59:59 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "This is a simple problem.";
+    return 0;
+}
+
