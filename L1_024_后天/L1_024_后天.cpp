1d641c05daa9b5b9d2a1e672655b955473ee0d11
ElonKou <elonkou@ktime.cc>
2018-01-21 11:59:31 +0800

    Add 24/38

---
+++ L1_024_后天/L1_024_后天.cpp
@@ -0,0 +1,21 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_024_后天.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Sun 21 Jan 2018 11:49:39 AM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int today = 0;
+    cin >> today;
+    if((today+2)%7 == 0){
+        cout << 7;
+    }else{
+        cout << (today+2)%7;
+    }
+    return 0;
+}
+
---
+++ L1_038_新世界/L1_038_新世界.cpp
@@ -0,0 +1,16 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_038_新世界.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Sun 21 Jan 2018 11:56:29 AM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    cout << "Hello New World";
+    return 0;
+}
+
