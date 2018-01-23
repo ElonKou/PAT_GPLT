4606c7e4ca4351fc445028f29c0ebdde7c3d9fe7
ElonKou <elonkou@ktime.cc>
2018-01-23 23:59:07 +0800

    some

---
+++ L1_028_判断素数/L1_028_判断素数.cpp
@@ -0,0 +1,51 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_028_判断素数.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Tue 23 Jan 2018 11:03:32 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int isSu(long number){
+    if(number %2 == 0&& number !=2 || number == 1){
+        return 0;
+    }else if(number == 2 ){
+        return 1;
+    }else{
+        //判断是否是素数
+        for(long i = 2;i < number/2;i ++){
+            if(number%i == 0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+int main(){
+    int num;
+    cin >> num;
+    int result[num] = {0};
+    long number;
+
+    for(int i = 0;i<num;i++){
+        cin >> number;
+        result[i] = isSu(number);
+    }
+    for(int i=0;i<num-1;i++){
+        if(result[i] == 1){
+            cout << "Yes" << endl;
+        }else{
+            cout << "No" << endl;
+        }
+    }
+    if(result[num-1] == 1){
+        cout << "Yes";
+    }else{
+        cout << "No";
+    }
+    return 0;
+}
+
