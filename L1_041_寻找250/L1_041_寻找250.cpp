a691f70967fe72caf133f3b9b239d2847335309f
ElonKou <elonkou@ktime.cc>
2018-01-23 23:02:00 +0800

    Add 250.

---
+++ L1_041_寻找250/L1_041_寻找250.cpp
@@ -0,0 +1,22 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_041_寻找250.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Tue 23 Jan 2018 10:53:46 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cin >> number;
+    int count = 1;
+    while(number != 250){
+        count ++;
+        cin >> number;
+    }
+    cout << count;
+    return 0;
+}
+
