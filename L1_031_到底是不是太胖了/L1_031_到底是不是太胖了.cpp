74fdd36703612344cd44ebbd42117c59e7583934
ElonKou <elonkou@ktime.cc>
2018-01-27 22:16:30 +0800

    Add L1_031

---
+++ L1_031_到底是不是太胖了/L1_031_到底是不是太胖了.cpp
@@ -0,0 +1,53 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_031_到底是不是太胖了.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Sat 27 Jan 2018 08:02:37 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int isPang(int height,int weight){
+    int res = 0;
+    float standard = (height-100)*0.9*2;
+    if(abs(weight-standard)/standard > 0.1 && weight > standard){
+        res = 1;
+    }
+    if(abs(weight-standard)/standard > 0.1 && weight < standard){
+        res = -1;
+    }
+    return res;
+}
+
+int main(){
+    // 到底是不是太胖了
+    int num;
+    cin >> num;
+    int result[num] = {0};
+    int height;
+    int weight;
+    for(int i = 0;i < num ;i++){
+        //
+        cin >> height;
+        cin >> weight;
+        result[i] = isPang(height,weight);
+    }
+    for(int i = 0; i< num;i++){
+        if(result[i] == 0){
+            cout << "You are wan mei!";
+        }
+        if(result[i] == 1){
+            cout << "You are tai pang le!";
+        }
+        if(result[i] == -1){
+            cout << "You are tai shou le!";
+        }
+        if(i != num -1){
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
