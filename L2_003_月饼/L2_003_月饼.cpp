c0dcffd318d28ff297c29940736f5cde0d92c736
ElonKou <elonkou@ktime.cc>
2018-02-15 18:11:10 +0800

    003

--- L2_003_月饼/L2_003_月饼.cpp
+++ L2_003_月饼/L2_003_月饼.cpp
@@ -6,40 +6,94 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #include <iostream>
-#include <algorithm>
+#include <iomanip>
+#include <cmath>
+
 using namespace std;
 
-typedef struct data{
+typedef struct{
 	int amount;
 	int maxNeed;
+	long double aver;
+	int order;
 }Data;
 
-void sort(){
-	//
+Data* findMax(Data *info,int count){
+	// find the max average price Data object and return it.
+	Data *max = &info[0];
+	int j = 0;
+	while(max -> order != 0){
+		max = &info[j++];
+	}
+	for(int i = 0;i < count;i++){
+		if(info[i].order == 0 && max->aver < info[i].aver){
+			max = &info[i];
+		}
+	}
+	max -> order = 1;
+	return max;
 }
 
-void calculate(int count,int sum){
-	//
+int checkFull(Data *info,int count){
+	int status = 0;
+	for(int i = 0;i < count ;i++){
+		if(info[i].order == 1){
+			status++;
+		}
+	}
+	if(status == count){
+		status = 1;
+	}else{
+		status = 0;
+	}
+	return status;
 }
 
-template <int count>
-void inputInfo(Data (&info)[count]){
-	// input the price.
-	for(int i = 0;i < count;i++){
-		cin >> info[0][i];
+void calculate(Data *info,int count,int sum){
+	long double price = 0.0;
+	int need = 0;
+	while(sum > 0){
+		if(checkFull(info,count) == 1){
+			break;
+		}
+		Data *data = findMax(info,count);
+		need = min(data -> amount , sum);
+		sum -= need;
+		price += data -> aver * need;
+		
+		//cout << "max:" << data -> amount << "\tneed:" << need << "\tsum:" << sum << endl;
 	}
-	// input the mount of goods.
-	for(int i = 0;i < count;i++){
-		cin >> info[1][i];
+	if(price > 0){
+		cout << setprecision(2) << std::fixed << price;
+	}else{
+		cout << 0;
 	}
+}
+
+void inputInfo(Data *info,int count){
+	for(int i = 0;i < count;i++){
+		cin >> info[i].amount;
+		info[i].order = 0;
+	}
+	for(int i = 0;i < count;i++){
+		cin >> info[i].maxNeed;
+		info[i].aver = info[i].maxNeed*1.0/info[i].amount;
+	}
+	// output
+	//for(int i = 0; i< count ;i++){
+		//cout<< "amount:" << info[i].amount 
+			//<< "\tMaxNeed:" << info[i].maxNeed
+			//<< "\taver:" << info[i].aver
+			//<< endl;
+	//}
 }
 
 int main(){
 	int count,sum;
 	cin >> count >> sum;
 	Data info[count];
-	inputInfo(info);
-	calculate(count,sum);
+	inputInfo(info,count);
+	calculate(info,count,sum);
     return 0;
 }
 
