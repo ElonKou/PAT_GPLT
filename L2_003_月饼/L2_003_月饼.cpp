559f77d44d39903d066d7ea2eb5a24edb3ddb1ac
ElonKou <elonkou@ktime.cc>
2018-02-16 16:19:14 +0800

    :bug:fixed a float bug.

--- L2_003_月饼/L2_003_月饼.cpp
+++ L2_003_月饼/L2_003_月饼.cpp
@@ -7,85 +7,47 @@
 
 #include <iostream>
 #include <iomanip>
-#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 typedef struct{
-	int amount;
-	int maxNeed;
-	long double aver;
-	int order;
+	float amount;
+	float maxNeed;
+	double aver;
 }Data;
 
-Data* findMax(Data *info,int count){
-	// find the max average price Data object and return it.
-	Data *max = &info[0];
-	int j = 0;
-	while(max -> order != 0){
-		max = &info[j++];
-	}
-	for(int i = 0;i < count;i++){
-		if(info[i].order == 0 && max->aver < info[i].aver){
-			max = &info[i];
-		}
-	}
-	max -> order = 1;
-	return max;
+int com(Data a,Data b){
+	return a.aver > b.aver;
 }
 
-int checkFull(Data *info,int count){
-	int status = 0;
-	for(int i = 0;i < count ;i++){
-		if(info[i].order == 1){
-			status++;
-		}
-	}
-	if(status == count){
-		status = 1;
-	}else{
-		status = 0;
-	}
-	return status;
+float mymin(int a,float b){
+	return (a<b)?(float)a:b;
 }
 
 void calculate(Data *info,int count,int sum){
-	long double price = 0.0;
-	int need = 0;
-	while(sum > 0){
-		if(checkFull(info,count) == 1){
+	sort(info,info+count,com);
+	double price = 0.0;
+	for(int i = 0;i < count;i++){
+		if(sum > 0){
+			float need = mymin(sum,info[i].amount);
+			sum -= need;
+			price += need * 1.0 * info[i].maxNeed/info[i].amount;
+		}else{
 			break;
 		}
-		Data *data = findMax(info,count);
-		need = min(data -> amount , sum);
-		sum -= need;
-		price += data -> aver * need;
-		
-		//cout << "max:" << data -> amount << "\tneed:" << need << "\tsum:" << sum << endl;
 	}
-	if(price > 0){
-		cout << setprecision(2) << std::fixed << price;
-	}else{
-		cout << 0;
-	}
+	cout << setprecision(2) << std::fixed << price;
 }
 
 void inputInfo(Data *info,int count){
 	for(int i = 0;i < count;i++){
 		cin >> info[i].amount;
-		info[i].order = 0;
 	}
 	for(int i = 0;i < count;i++){
 		cin >> info[i].maxNeed;
 		info[i].aver = info[i].maxNeed*1.0/info[i].amount;
 	}
-	// output
-	//for(int i = 0; i< count ;i++){
-		//cout<< "amount:" << info[i].amount 
-			//<< "\tMaxNeed:" << info[i].maxNeed
-			//<< "\taver:" << info[i].aver
-			//<< endl;
-	//}
 }
 
 int main(){
