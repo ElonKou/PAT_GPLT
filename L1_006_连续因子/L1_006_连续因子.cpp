618772a9bfb9da9d4abe6fd1839b59c22ab5c3a8
ElonKou <elonkou@ktime.cc>
2018-02-17 10:28:43 +0800

    :bug:fixed bug of output.

--- L1_006_连续因子/L1_006_连续因子.cpp
+++ L1_006_连续因子/L1_006_连续因子.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 long long multi(int start,int end){
 	long long res = 1;
-	for(;start < end;start++){
+	for(;start <= end;start++){
 		res *= start;
 	}
 	return res;
@@ -20,9 +20,9 @@ long long multi(int start,int end){
 
 void print(int index,int len,int num){
 	long long result = 1;
-	//cout << len - index +1<< endl;
-	//cout << index << " " << len << endl;
-	for(int i = index;i<len+1;i++){
+	cout << len - index+1 << endl;
+	//cout << index << "-" << len << ":\t";
+	for(int i = index;i <= len;i++){
 		result *= i;
 		if(i != index){
 			cout << "*" << i;
@@ -30,11 +30,11 @@ void print(int index,int len,int num){
 			cout << i;
 		}
 	}
-	cout << " = " << result;
-	if(num % result == 0){
-		cout << "是";
-	}
-	cout << endl;
+	//cout << " = " << result;
+	//if(num % result == 0){
+		//cout << "是";
+	//}
+	//cout << endl;
 }
 
 int main(){
@@ -42,15 +42,25 @@ int main(){
 	// the most shortest array containes longest number set.
     int num,top;
 	cin >> num;
-	top = sqrt(num+1);
-	int loop = 1;
+	top = sqrt(num) + 1;
+	int count = MAX_;
 	int isOk = 0;
-	for(int len = MAX_;len > 1 ;len--,loop++){
-		for(int index = 2;index < loop+2;index ++){
+	for(int len = MAX_;len >= 0 ;len--){
+		for(int index = 2;index <= top;index ++){
 			//cout << index << " " << index - loop + MAX_ -1<< endl;
-			int end = index - loop + MAX_ -1;
+			int end = index + len;
 			long long result = multi(index,end);
-				print(index,end,num);
+			if(result <= num && result > 0){
+				if(num % result == 0 && isOk == 0){
+					print(index,end,num);
+					return 0;
+				}
+			}
+
+			//if(num % result == 0 ){
+				//print(index,end,num);
+				//isOk = 1;
+			//}
 			//if(num % result == 0 && result !=1){
 				//print(index,end);
 				//isOk = 1;
@@ -60,6 +70,7 @@ int main(){
 			//}
 		}
 	}
+	cout << "1\n" << num;
 	return 0;
 }
 
