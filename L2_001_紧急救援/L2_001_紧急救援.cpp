f87fc536209cc49d48c51c1353fcc49d533d6d41
ElonKou <elonkou@ktime.cc>
2018-02-18 21:09:10 +0800

    ...nothing...

--- L2_001_紧急救援/L2_001_紧急救援.cpp
+++ L2_001_紧急救援/L2_001_紧急救援.cpp
@@ -2,76 +2,66 @@
 // File:  L2_001_紧急救援.cpp
 // Author :elonkou
 // E-mail :elonkou@ktime.cc
-// Date   :Mon 29 Jan 2018 11:12:58 AM CST
+// Date   :Sun 18 Feb 2018 05:51:38 PM CST
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #include <iostream>
 using namespace std;
 
-typedef struct road{
-   int start;       // 开始id
-   int end;         // 结束id
-   int length;      // 道路长度
-   road *next;
-}road;
+struct road{
+	int start,end,length;
+};
 
-typedef struct{
-    int id;         // id
-    int count;      // 救援队数量
-    road *roads;    // 指向的道路数量
-    road *endRoad;
-}city;
+struct city{
+	int aidCnt;
+};
 
-void calculate(city cities[],int cityNum,int roadNum,int start, int end){
-    // 计算路径数量和人数
-    int dist[cityNum] = {0};        // 使用dijkstra算法进行求解最短路径
-    int path[cityNum];              // 记录经过的路径
-    //int count// 
-    for(int i =0;i < cityNum;i++){
-        //for(){
-            ////
-        //}
-    }
+void print(road r[500][500]){
+	for(int i = 0;i < 4;i++){
+		for(int j = 0;j < 4;j++){
+			if(r[i][j].length == 0){
+				cout << "*";
+			}else{
+				cout << r[i][j].length;
+			}
+		}
+		cout << endl;
+	},
+}
+
+void min(road *info,int length,int &start,int &end,int data){
+	// 计算条路径上的最小值,对其中的出发地点进行修改
+	for(int i = 0;i < length;i++){
+		if(info[i].length != 0 && info[i].length < data){
+			start = 
+		}
+	}
+}
+
+void dijkstra(int start,int end,road (*info)[500][500],int cityCnt){
+	for(){
+		//
+	}
 }
 
 int main(){
-    // 紧急救援
-    int cityNum;
-    int roadNum;
-    int start,end;
-    cin >> cityNum >> roadNum >> start >> end;
-    // 获得所有的城市信息的输入
-    // 并且将所有城市呢道路信息的初始化
-    city cities[cityNum];
-    for(int i = 0; i < cityNum;i++){
-        cities [i].id = i;
-        cin >> cities[i].count;
-        road *r = (road *)malloc(sizeof(road));     // 创建一个新的空指针指向初始位置
-        r -> next = NULL;
-        cities[i].roads = r;
-        cities[i].endRoad = r;
-    }
-    // 获得所有的road信息
-    for(int i = 0 ;i < roadNum;i++){
-        int st,en,le;
-        cin >> st >> en >> le;
-        
-        road *r = (road *)malloc(sizeof(road));
-        r -> start = st;
-        r -> end = en;
-        r -> length = le;
-        r -> next =NULL;
-        cities[st].endRoad->next = r;
-        cities[st].endRoad = r;
-        
-        r = (road *)malloc(sizeof(road));
-        r -> start = en;
-        r -> end = st;
-        r -> length = le;
-        r -> next =NULL;
-        cities[en].endRoad->next = r;
-        cities[en].endRoad = r;
-    }
-    return 0;
+	int cityCnt;
+	int roadCnt;
+	int start,end;
+	cin >> cityCnt >> roadCnt >> start >> end;
+	road info[500][500] = {0};
+    city cities[cityCnt] = {{0}};
+	// input 
+	for(int i = 0;i < cityCnt;i++){
+		cin >> cities[i].aidCnt;
+	}
+	for(int i = 0;i < roadCnt;i++){
+		road r;
+		cin >> r.start >> r.end >> r.length;
+		info[r.start][r.end] = info[r.end][r.start] = r;
+	}
+	//print(info);
+	dijkstra(start,end,info,cityCnt);
+	return 0;
 }
 
