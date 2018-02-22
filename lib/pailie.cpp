a10c72ac5186c7dc0255210f67444a99a8023d81
ElonKou <elonkou@ktime.cc>
2018-02-22 17:46:01 +0800

    Add lib.

---
+++ lib/pailie.cpp
@@ -0,0 +1,23 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  pailie.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Thu 22 Feb 2018 02:19:07 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+#define MAXN 11
+
+
+using namespace std;
+
+void pailie(int n,int m){
+	//
+}
+
+int main(){
+	int num;
+	cin >> num;
+    return 0;
+}
+
