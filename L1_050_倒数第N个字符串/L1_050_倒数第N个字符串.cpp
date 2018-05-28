50e1bd576d78c9e36de11a3d0ad65f1a4c218706
ElonKou <elonkou@ktime.cc>
2018-05-28 21:57:53 +0800

    Add 050

---
+++ L1_050_倒数第N个字符串/L1_050_倒数第N个字符串.cpp
@@ -0,0 +1,32 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_050_倒数第N个字符串.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Sat 26 May 2018 10:37:03 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int len;
+    long num;
+    cin >> len;
+    cin >> num;
+    num --;
+    int temp = 0;
+    char cc[len];
+    while(len > 1){
+        len --;
+        temp = int(num / pow(26, len));
+        num -= int(pow(26, len)) * temp;
+        temp = 25 - temp;
+        printf("%c",char(temp + 97));
+    };
+    temp = 25 - num;
+    printf("%c",char(temp + 97));
+    return 0;
+}
+
