c53026053aa6b7d43bb7a04d25877732b5a675e4
ElonKou <elonkou@ktime.cc>
2018-05-29 20:25:32 +0800

    Add 051

---
+++ L1_051_打折/L1_051_打折.cpp
@@ -0,0 +1,19 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_051_打折.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Sat 26 May 2018 11:51:23 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int price;
+    int zhe;
+    cin >> price >> zhe;
+    printf("%.2f", price * zhe * 1.0 / 10);
+    return 0;
+}
