8a149292911fa7429dc2f72d57c36583f2bc0f0a
ElonKou <elonkou@ktime.cc>
2018-05-31 07:16:09 +0800

    Add 052

---
+++ L1_052_2018我们要赢/L1_052_2018我们要赢.cpp
@@ -0,0 +1,16 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_052_2018我们要赢.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Sun 27 May 2018 12:02:15 AM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "2018" << endl;
+    cout << "wo3 men2 yao4 ying2 !";
+    return 0;
+}
+
