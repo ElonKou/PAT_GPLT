964bee99d836763c3df43ced6d3f813b6febba30
ElonKou <elonkou@ktime.cc>
2018-06-03 11:48:35 +0800

    Add 053

---
+++ L1_053_电子汪/L1_053_电子汪.cpp
@@ -0,0 +1,20 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  L1_053_电子汪.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Sun 27 May 2018 12:04:30 AM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    for (int i = 0; i < a + b; i++)
+    {
+        cout << "Wang!";
+    }
+    return 0;
+}
